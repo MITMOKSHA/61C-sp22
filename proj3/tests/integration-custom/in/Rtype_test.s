4be33b2cba4ad613f0944376f39ed0a0505bbb34
Moksha <490135851@qq.com>
2022-04-02 18:09:27 +0800

    proj3 task 5 6 7 done

---
+++ proj3/tests/integration-custom/in/Rtype_test.s
@@ -0,0 +1,13 @@
+add	  s0, x0, x1
+sub	  s1, x0, x1
+and	  s1, x5, x6
+or	  s1, x5, x6
+xor	  s1, x5, x6
+sll	  s1, x5, x6
+srl	  s1, x5, x6
+sra	  s1, x5, x6
+slt	  s1, x5, x6
+mul   s1, x5, x6
+mulh  s1, x5, x6
+mulhu s1, x5, x6
+
